3bf327c32ce0322bef98aca2c1f7321f0af88083
luanguedes7 <lcrisley98@gmail.com>
2023-06-15 11:17:27 -0300

    Declaração das Controladoras de Serviço (Back-end)

--- include/Controladoras.hpp
+++ include/Controladoras.hpp
@@ -1,5 +1,33 @@
 #ifndef CONTROLADORAS_HPP
 #define CONTROLADORAS_HPP
 
+//Controladoras de Servico
+
+class CntrServicoAutenticacao: public IServicoAutenticacao{
+    public:
+        bool autenticar(Matricula, Senha);
+};
+
+class CntrServicoDesenvolvedor: public IServicoDesenvolvedor{
+    public:
+        bool cadastrar(Desenvolvedor);
+        bool descadastrar(Matricula);
+        bool editar(Desenvolvedor);
+        bool visualizar(Desenvolvedor*);
+};
+
+class CntrServicoTeste: public IServicoTeste{
+    public:
+        bool visualizar(Teste*);
+        bool cadastrar(Teste);
+        bool editar(Teste);
+        bool descadastrar(Teste);
+};
+
+class CntrServicoCasoDeTeste: public IServicoCasoDeTeste{
+        bool visualizar(CasoDeTeste*);
+        bool cadastrar(CasoDeTeste);
+        bool descadastrar(CasoDeTeste);
+};
 
 #endif  //  CONTROLADORAS_HPP
