a3367859ac16320d5e276546aa31cc4ebf325b00
Victor André <victorandre957@gmail.com>
2023-07-01 16:59:38 -0300

    faz funcionar

--- include/Interfaces.hpp
+++ include/Interfaces.hpp
@@ -70,7 +70,7 @@ class IServicoTeste {
 class IServicoCasoDeTeste {
     public:
         virtual bool consultar(CasoDeTeste*) = 0;
-        virtual bool cadastrar(CasoDeTeste, Codigo);
+        virtual bool cadastrar(CasoDeTeste, Codigo) = 0;
         virtual bool editar(CasoDeTeste) = 0;
         virtual bool descadastrar(Codigo) = 0;
         virtual ~IServicoCasoDeTeste(){}
